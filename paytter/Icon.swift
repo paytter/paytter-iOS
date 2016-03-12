e51ff7fc8b9605c89bca02326b9d297892fd51e0
Kengo Yokoyama <rbken.lo.ovl@gmail.com>
2016-03-12 16:39:58 +0900

    :heavy_plus_sign: アイコン管理クラスを追加

---
+++ paytter/Icon.swift
@@ -0,0 +1,13 @@
+//
+//  Icon.swift
+//  paytter
+//
+//  Created by Kengo Yokoyama on 2016/03/12.
+//  Copyright © 2016年 ITF. All rights reserved.
+//
+
+import UIKit
+
+class Icon: NSObject {
+    static let kClose = "\u{f00d}"
+}
