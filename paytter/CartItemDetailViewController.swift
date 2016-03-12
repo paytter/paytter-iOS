3e85f73fae06e43e400f4861b4d180b931121569
Kengo Yokoyama <rbken.lo.ovl@gmail.com>
2016-03-12 16:27:10 +0900

    :heavy_plus_sign: 商品詳細画面のデザインを設定

--- paytter/CartItemDetailViewController.swift
+++ paytter/CartItemDetailViewController.swift
@@ -10,26 +10,20 @@ import UIKit
 
 class CartItemDetailViewController: UIViewController {
 
+    @IBOutlet private weak var nameLabel: UILabel!
+    @IBOutlet private weak var quantityLabel: UILabel!
+    @IBOutlet private weak var priceLabel: UILabel!
+    @IBOutlet private weak var productImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction private func didTouchCancelButton(sender: UIButton) {
+        print("didTouchCancelButton")
     }
-    */
-
+    
+    @IBAction private func didTouchStepper(sender: UIStepper) {
+        print("didTouchStepper")
+    }
 }
