76441d0c395ae119a667dbb86a4ea19f50dab753
Kengo Yokoyama <rbken.lo.ovl@gmail.com>
2016-03-12 13:43:57 +0900

    :heavy_plus_sign: UIクラスを追加

---
+++ paytter/CartItemCell.swift
@@ -0,0 +1,24 @@
+//
+//  CartItemCell.swift
+//  paytter
+//
+//  Created by Kengo Yokoyama on 2016/03/12.
+//  Copyright © 2016年 ITF. All rights reserved.
+//
+
+import UIKit
+
+class CartItemCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
