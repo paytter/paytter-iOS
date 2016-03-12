33ad4fa5384d979b0ef0e852d9fc88c6bdf9177a
Kengo Yokoyama <rbken.lo.ovl@gmail.com>
2016-03-12 16:07:38 +0900

    :heavy_plus_sign: 商品モデルクラスを追加

---
+++ paytter/Product.swift
@@ -0,0 +1,69 @@
+//
+//  Product.swift
+//  paytter
+//
+//  Created by Kengo Yokoyama on 2016/03/12.
+//  Copyright © 2016年 ITF. All rights reserved.
+//
+
+import SwiftyJSON
+
+class Product: NSObject {
+    var price: Int?
+    var category: String?
+    var detail: Detail?
+    var imageUrl: String?
+    var itemId: String?
+    var score: String?
+    var sites: Site?
+    
+    init(json: JSON) {
+        price = json["price"].int
+        category = json["candidates"]["category"].string
+        imageUrl = json["candidates"]["imageUrl"].string
+        itemId = json["candidates"]["itemId"].string
+        score = json["candidates"]["score"].string
+        detail = Detail.getDetail(json["candidates"])
+        sites = Site.getSite(json["candidates"])
+    }
+    
+    class Detail {
+        var brand: String?
+        var itemName: String?
+        var maker: String?
+        var quantity: String?
+        var releaseDate: String?
+        
+        init (json: JSON) {
+            brand = json["brand"].string
+            itemName = json["itemName"].string
+            maker = json["maker"].string
+            quantity = json["quantity"].string
+            releaseDate = json["releaseDate"].string
+        }
+        
+        class func getDetail(json: JSON) -> Detail {
+            let detail = Detail(json: json["detail"])
+            
+            return detail
+        }
+    }
+    
+    class Site {
+        var imageUrl: String?
+        var title: String?
+        var url: String?
+        
+        init (json: JSON) {
+            imageUrl = json["imageUrl"].string
+            title = json["title"].string
+            url = json["url"].string
+        }
+        
+        class func getSite(json: JSON) -> Site {
+            let site = Site(json: json["site"])
+            
+            return site
+        }
+    }
+}
