798855fbd954c493db22a487d11350d982a708c4
Kengo Yokoyama <rbken.lo.ovl@gmail.com>
2016-03-12 23:46:54 +0900

    :heavy_plus_sign: Beaconスキャンクラスを追加

---
+++ paytter/BeaconScanner.swift
@@ -0,0 +1,20 @@
+//
+//  BeaconScanner.swift
+//  paytter
+//
+//  Created by Kengo Yokoyama on 2016/03/12.
+//  Copyright © 2016年 ITF. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+class BeaconScanner: NSObject {
+    
+    override init() {
+    }
+}
+
+extension BeaconScanner: CLLocationManagerDelegate {
+    
+}
