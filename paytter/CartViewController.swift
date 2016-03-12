3edec4cb47ae8362ea2ea700c961ba33119bfc2d
Kengo Yokoyama <rbken.lo.ovl@gmail.com>
2016-03-12 16:13:41 +0900

    :heavy_plus_sign: カート確認画面を実装

--- paytter/CartViewController.swift
+++ paytter/CartViewController.swift
@@ -10,9 +10,43 @@ import UIKit
 
 class CartViewController: UIViewController {
 
+    private var products = [Product]() {
+        didSet {
+            tableView.reloadData()
+        }
+    }
+    
+    @IBOutlet private weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         navigationController?.topViewController?.title = "商品確認"
+    }
+}
+
+extension CartViewController: UITableViewDataSource {
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return products.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(CartItemCell.kIdentifier, forIndexPath: indexPath) as! CartItemCell
+        cell.configure(product: products[indexPath.row])
+        return cell
+    }
+}
+
+extension CartViewController: UITableViewDelegate {
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return UITableViewAutomaticDimension
+    }
+    
+    func tableView(tableView: UITableView, estimatedHeightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return UITableViewAutomaticDimension
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        // TODO: 詳細画面出す
     }
 }
