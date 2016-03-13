7c3a58bfb16c13b467e9c36e0cf1ab3a4ac86a6c
Kengo Yokoyama <rbken.lo.ovl@gmail.com>
2016-03-13 09:43:58 +0900

    :heavy_plus_sign: 画像認識部にBeaconスキャンを追加

--- paytter/ImageScanViewController.swift
+++ paytter/ImageScanViewController.swift
@@ -10,14 +10,26 @@ import UIKit
 
 class ImageScanViewController: UIViewController {
 
+    @IBOutlet private weak var rssiLabel: UILabel!
+    @IBOutlet private weak var distanceLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        APIManager.sharedManager.getProduct(storeId: 1, eanId: nil, isbnId: nil, itemIds: "food_0000121261", completion: {
-            (product: Product) -> Void in
-            let scanItemDetailViewController = ViewControllerFactory.scanItemDetailViewController()
-            scanItemDetailViewController.product = product
-            self.presentViewController(scanItemDetailViewController, animated: true, completion: nil)
-        })
+        BeaconScanner.sharedManager.delegate = self        
+//        APIManager.sharedManager.getProduct(storeId: 1, eanId: nil, isbnId: nil, itemIds: "food_0000121261", completion: {
+//            (product: Product) -> Void in
+//            let scanItemDetailViewController = ViewControllerFactory.scanItemDetailViewController()
+//            scanItemDetailViewController.product = product
+//            self.presentViewController(scanItemDetailViewController, animated: true, completion: nil)
+//        })
+    }
+}
+
+extension ImageScanViewController: BeaconScannerDelegate {
+    func didRangeBeacon(rssi rssi: String, distance: String) {
+        print(rssi, distance)
+        rssiLabel.text = "RSSI: \(rssi)"
+        distanceLabel.text = "distance: \(distance)"
     }
 }
