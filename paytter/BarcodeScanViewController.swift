9e992fc3181a9a748428df69086f0443655a5c2f
Kengo Yokoyama <rbken.lo.ovl@gmail.com>
2016-03-13 10:35:17 +0900

    :sparkles: リファクタリング

--- paytter/BarcodeScanViewController.swift
+++ paytter/BarcodeScanViewController.swift
@@ -9,12 +9,12 @@
 import UIKit
 import AVFoundation
 
-class BarcodeScanViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
+class BarcodeScanViewController: UIViewController {
 
-    var captureSession: AVCaptureSession!
-    var previewLayer: AVCaptureVideoPreviewLayer!
+    private var captureSession: AVCaptureSession!
+    private var previewLayer: AVCaptureVideoPreviewLayer!
     
-    @IBOutlet weak var scanView: UIView!
+    @IBOutlet private weak var scanView: UIView!
     @IBOutlet private weak var rssiLabel: UILabel!
     @IBOutlet private weak var distanceLabel: UILabel!
     
@@ -34,26 +34,19 @@ class BarcodeScanViewController: UIViewController, AVCaptureMetadataOutputObject
 //        })
     }
 
-    func failed() {
-        let ac = UIAlertController(title: "スキャン不可", message: "この端末はカメラでのスキャンがサポートされていません!", preferredStyle: .Alert)
-        ac.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
-        presentViewController(ac, animated: true, completion: nil)
-        captureSession = nil
-    }
-
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
 
-        if (captureSession?.running == false) {
-            captureSession.startRunning();
+        if captureSession?.running == false {
+            captureSession.startRunning()
         }
     }
 
     override func viewWillDisappear(animated: Bool) {
         super.viewWillDisappear(animated)
 
-        if (captureSession?.running == true) {
-            captureSession.stopRunning();
+        if captureSession?.running == true {
+            captureSession.stopRunning()
         }
     }
 
@@ -69,16 +62,16 @@ class BarcodeScanViewController: UIViewController, AVCaptureMetadataOutputObject
             return
         }
 
-        if (captureSession.canAddInput(videoInput)) {
+        if captureSession.canAddInput(videoInput) {
             captureSession.addInput(videoInput)
         } else {
-            failed();
-            return;
+            failed()
+            return
         }
 
         let metadataOutput = AVCaptureMetadataOutput()
 
-        if (captureSession.canAddOutput(metadataOutput)) {
+        if captureSession.canAddOutput(metadataOutput) {
             captureSession.addOutput(metadataOutput)
 
             metadataOutput.setMetadataObjectsDelegate(self, queue: dispatch_get_main_queue())
@@ -88,43 +81,49 @@ class BarcodeScanViewController: UIViewController, AVCaptureMetadataOutputObject
             return
         }
 
-        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession);
-        previewLayer.frame = view.layer.bounds;
-        previewLayer.videoGravity = AVLayerVideoGravityResizeAspectFill;
-        scanView.layer.addSublayer(previewLayer);
-        
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer.frame = view.layer.bounds
+        previewLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
+        scanView.layer.addSublayer(previewLayer)
         scanView.layer.addSublayer(rssiLabel.layer)
         scanView.layer.addSublayer(distanceLabel.layer)
     }
 
-    // MARK: AVCaptureMetadataOutputObjectsDelegateのメソッド
+    private func foundCode(code: String) {
+        print(code)
+        // NOTE: ここにバーコードを読み取り成功時の処理を書きます
+    }
+    
+    private func failed() {
+        let ac = UIAlertController(title: "スキャン不可", message: "この端末はカメラでのスキャンがサポートされていません!", preferredStyle: .Alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+        presentViewController(ac, animated: true, completion: nil)
+        captureSession = nil
+    }
 
+    override func supportedInterfaceOrientations() -> UIInterfaceOrientationMask {
+        return .Portrait
+    }
+}
+
+extension BarcodeScanViewController: AVCaptureMetadataOutputObjectsDelegate {
     func captureOutput(captureOutput: AVCaptureOutput!, didOutputMetadataObjects metadataObjects: [AnyObject]!, fromConnection connection: AVCaptureConnection!) {
         captureSession.stopRunning()
-
+        
         if let metadataObject = metadataObjects.first {
             let readableObject = metadataObject as! AVMetadataMachineReadableCodeObject;
-
+            
             AudioServicesPlaySystemSound(SystemSoundID(kSystemSoundID_Vibrate))
             foundCode(readableObject.stringValue);
         }
-
+        
         dismissViewControllerAnimated(true, completion: nil)
-
+        
         let delay = 1 * Double(NSEC_PER_SEC)
         let time  = dispatch_time(DISPATCH_TIME_NOW, Int64(delay))
         dispatch_after(time, dispatch_get_main_queue(), {
             self.captureSession.startRunning()
         })
-    }
-
-    func foundCode(code: String) {
-        print(code)
-        // NOTE: ここにバーコードを読み取り成功時の処理を書きます
-    }
-
-    override func supportedInterfaceOrientations() -> UIInterfaceOrientationMask {
-        return .Portrait
     }
 }
 
